28e7bed1675ffa8a55a2204df21bb5eaabcbc275
mirajp1 <mirajp1@gmail.com>
2015-06-09 23:08:31 +0530

    Fixed preview distortion in potrait and added focus mode as well as Async Task to save the image.

--- app/src/main/java/com/example/android/nsdchat/CameraView.java
+++ app/src/main/java/com/example/android/nsdchat/CameraView.java
@@ -1,8 +1,10 @@
 package com.example.android.nsdchat;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -18,6 +20,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Random;
 
 import static android.view.SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS;
@@ -30,52 +33,113 @@ public class CameraView extends Activity implements SurfaceHolder.Callback {
     Camera mCamera;
     SurfaceView mSurfaceView;
     SurfaceHolder mSurfaceHolder;
-    Bitmap bitmap;
     Button mCapture;
-    public String path = Environment.getDataDirectory().getAbsolutePath() + "/storage/emulated/0/Pictures/Cam";
+    //    public String path = Environment.getDataDirectory().getAbsolutePath() + "/storage/emulated/0/Pictures/Cam";
+    Camera.PictureCallback mPictureCallback;
+    private Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_view);
+
         mSurfaceView = (SurfaceView) findViewById(R.id.surface_camera);
         mSurfaceHolder = mSurfaceView.getHolder();
         mSurfaceHolder.addCallback(this);
         mSurfaceHolder.setType(SURFACE_TYPE_PUSH_BUFFERS);
         mPreviewRunning = false;
 
-        File mFolder = new File(path);
-        if(!mFolder.exists())
-            mFolder.mkdir();
+//        File mFolder = new File(path);
+//        if (!mFolder.exists())
+//            mFolder.mkdir();
         mCapture = (Button) findViewById(R.id.capture);
+
+        mPictureCallback = new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] bytes, Camera camera) {
+                Random generator = new Random();
+                int n = 1000;
+                n = generator.nextInt(n);
+
+                String fName = "Image-" + n + ".jpg";
+
+                File pictureFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/", fName);
+                try {
+                    FileOutputStream fos = new FileOutputStream(pictureFile);
+                    Log.e(LOG_TAG, pictureFile.toString());
+
+//                    mBitmap= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+//                    if(mBitmap!=null){
+//                        Log.e(LOG_TAG,"Bitmap not null.");
+//                        if (mBitmap.getWidth() > mBitmap.getHeight()) {
+//                            Matrix matrix = new Matrix();
+//                            matrix.postRotate(90);
+//                            mBitmap = Bitmap.createBitmap(mBitmap , 0, 0, mBitmap.getWidth(), mBitmap.getHeight(), matrix, true);
+//
+//                            bytes=mBitmap.get
+//                        }
+//                    }
+//                    else
+//                        Log.e(LOG_TAG, "Bitmap not null.");
+
+                    fos.write(bytes);
+
+                    fos.flush();
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    Log.e(LOG_TAG, e.getMessage());
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, e.getMessage());
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+
         mCapture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCamera.takePicture(null, null, new Camera.PictureCallback() {
+
+                mCamera.autoFocus(new Camera.AutoFocusCallback() {
                     @Override
-                    public void onPictureTaken(byte[] bytes, Camera camera) {
-                        Random generator = new Random();
-                        int n = 1000;
-                        n = generator.nextInt(n);
-                        String fName = "Image-" + n + ".jpg";
-                        File pictureFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/", fName);
-                        try {
-                            FileOutputStream fos = new FileOutputStream(pictureFile);
-                            Log.e(LOG_TAG, pictureFile.toString());
-                            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, fos);
-                            fos.flush();
-                            fos.close();
-                        } catch (FileNotFoundException e) {
-                            Log.e(LOG_TAG, e.getMessage());
-                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                        } catch (IOException e) {
-                            Log.e(LOG_TAG, e.getMessage());
-                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
+                    public void onAutoFocus(boolean b, Camera camera) {
+                        if (b == true) {
+//                            mCamera.takePicture(null, null, mPictureCallback);
+//                            mCamera.startPreview();
+                            new PictureTask().execute();
+
+                        } else
+                            Toast.makeText(getApplicationContext(), "Not Sharp Focussed", Toast.LENGTH_SHORT).show();
+
+                        if (mCamera.getParameters().getFocusMode() == Camera.Parameters.FOCUS_MODE_AUTO)
+                            mCamera.cancelAutoFocus();
                     }
                 });
+
+
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mCamera = Camera.open();
+        if (getOrientation() == Configuration.ORIENTATION_PORTRAIT)
+            mCamera.setDisplayOrientation(90);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mCamera != null) {
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
     }
 
     @Override
@@ -102,39 +166,131 @@ public class CameraView extends Activity implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        mCamera = Camera.open();
+//        mCamera = Camera.open();
+//        mCamera.setDisplayOrientation(90);
+
+//        try {
+//            mCamera.setPreviewDisplay(mSurfaceHolder);
+//        } catch (IOException e) {
+//            mCamera.release();
+//            mCamera = null;
+//        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int w, int h) {
+        if (mPreviewRunning) {
+            mCamera.stopPreview();
+        }
+
+        Camera.Parameters parameters = mCamera.getParameters();
+        List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
+
+        Camera.Size mPreviewSize = getOptimalPreviewSize(previewSizes, w, h);
+        parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+
+        if (parameters.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE))
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+        else if (parameters.getSupportedFocusModes().contains(Camera.Parameters.FLASH_MODE_AUTO))
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        mCamera.setParameters(parameters);
         try {
             mCamera.setPreviewDisplay(mSurfaceHolder);
         } catch (IOException e) {
             mCamera.release();
             mCamera = null;
         }
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int w, int h) {
-//        if (mPreviewRunning){
-//            mCamera.stopPreview();
-//        }
-//        Camera.Parameters p = mCamera.getParameters();
-//        List<Camera.Size> previewSizes = p.getSupportedPreviewSizes();
-//        Camera.Size previewSize = previewSizes.get(0);
-//        p.setPreviewSize(previewSize.width, previewSize.height);
-//        mCamera.setParameters(p);
-//        try{
-//            mCamera.setPreviewDisplay(surfaceHolder);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         mCamera.startPreview();
         mPreviewRunning = true;
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        mCamera.stopPreview();
-        mCamera.release();
-        mPreviewRunning = false;
-        mCamera = null;
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mPreviewRunning = false;
+            mCamera = null;
+        }
     }
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio = (double) h / w;
+
+        if (sizes == null) return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+    //temporary method put here
+    public int getOrientation() {
+        if (getResources().getDisplayMetrics().widthPixels > getResources().getDisplayMetrics().heightPixels) {
+            Toast t = Toast.makeText(this, "LANDSCAPE", Toast.LENGTH_SHORT);
+            t.show();
+            return Configuration.ORIENTATION_LANDSCAPE;
+        }
+
+        Toast t = Toast.makeText(this, "PORTRAIT", Toast.LENGTH_SHORT);
+        t.show();
+        return Configuration.ORIENTATION_PORTRAIT;
+    }
+
+
+    private class PictureTask extends AsyncTask<Void, String, Void> {
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mCamera.startPreview();
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+            Toast.makeText(getApplicationContext(), values[0], Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            mCamera.takePicture(null, null, mPictureCallback);
+
+            publishProgress("Image Captured");
+
+            try {
+                Thread.sleep(2000);     //Captured Image Preview for 2 seconds
+            } catch (InterruptedException ex) {
+                Log.e(LOG_TAG, "Interrupted: " + ex);
+            }
+
+            return null;
+        }
+    }
+
 }
